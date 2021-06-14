7a813621024bc13d25d19579a66cff86fb5a6b69
Jonathan Zak <jonathan.zak@mail.utoronto.ca>
2021-06-14 15:30:48 -0400

    changed port

--- server.py
+++ server.py
@@ -68,4 +68,4 @@ def featureUnvote():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, port=3000)
